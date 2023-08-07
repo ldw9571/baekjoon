e71246a98b0a05e2abe70901c54c4ac9a66eff0c
myblog <myblog0419@naver.com>
2023-08-07 23:10:01 +0900

    beckjoon 시작

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -13,18 +16,31 @@ public class Main {
         //신문 기사에 실려있는 참가자의 수가 몇명만큼 잘못되어 있는지 구하기
 
         //1제곱미터당 사람의 수
-        int countBy1Square = 0;
+        int countBy1Square = 5;
         //파티가 열렸던 곳의 넓이
-        int squareByParty = 0;
+        int squareByParty =20;
 
-        //기사에 실려있는 참가자의 수
-            int countByPeopleByArticle;
+        //기사 참가자수 5
+        int article1 = 99;
+        int article2 = 101;
+        int article3 = 1000;
+        int article4 = 0;
+        int article5 = 97;
 
-
+        //기사 참가자수 list에 담아줌
+        List<Integer> integers = articleCount(article1, article2, article3, article4, article5);
 
         //상근이가 생각하는 사람의 수
         int countbySangguen = countbySangguen(countBy1Square, squareByParty);
 
+        //상근이가 생각하는 사람의 수 - 기사에 실려있는 참가자의 수
+        List<Integer> variances = variances(countbySangguen, integers);
+
+        int i = 1;
+        for (Integer variance : variances) {
+            System.out.println("상근 - 기사"+i+" 차이 = " + variance);
+            i++;
+        }
     }
 
 
@@ -34,7 +50,7 @@ public class Main {
 
     //상근이가 생각하는 사람의 수
     public static int countbySangguen(int countBy1Square,int squareByParty){
-        int countBySangguen=0;
+        int countBySangguen;
 
         //인원수 조건 확인
         boolean trueByPeople = conditionBySquareBypeople(countBy1Square);
@@ -64,6 +80,32 @@ public class Main {
         }
         System.out.println("파티의 넓이를 정확하게 입력해주세요");
         return false;
+    }
+
+    //조건에 맞는 5개의 기사 갯수 list
+    public static List<Integer> articleCount(int article1,int article2,int article3,int article4,int article5){
+        List countByPeopleByArticle = new ArrayList<>();
+
+        countByPeopleByArticle.add(article1);
+        countByPeopleByArticle.add(article2);
+        countByPeopleByArticle.add(article3);
+        countByPeopleByArticle.add(article4);
+        countByPeopleByArticle.add(article5);
+
+        return countByPeopleByArticle;
+    }
+
+    //기사 - 상근 구하기
+    public static List<Integer> variances(int countbySangguen, List<Integer> integers){
+        List variances = new ArrayList<>();
+
+        for (Integer integer : integers){
+            int variance = integer - countbySangguen;
+//            int abs = Math.abs(variance);
+            variances.add(variance);
+        }
+
+        return variances;
     }
 
 }
