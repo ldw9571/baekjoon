bbbc6886d95a48661d62bf92c73f0a31c27a933b
myblog <myblog0419@naver.com>
2023-08-09 18:12:14 +0900

    2846번 해결

--- src/baek2846.java
+++ src/baek2846.java
@@ -1,23 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class baek2846 {
     public static void main(String[] args) {
 
         //수열의 크기
-        int N = 0;
+        int N = 8;
         // N개의 양의정수
-        int pi1 = 0, pi2 = 0, pi3 = 0, pi4 = 0, pi5 = 0;
-        List<Integer> integers = addList(pi1, pi2, pi3, pi4, pi5);
+        int pi1 = 12, pi2 = 100, pi3 = 1, pi4 = 2, pi5 = 24,pi6 = 68,pi7 = 1,pi8 = 2;
+        List<Integer> integers = addList(pi1, pi2, pi3, pi4, pi5,pi6,pi7,pi8);
         boolean b = conditionN(N);
+        System.out.println("b = " + b);
         boolean b1 = conditionPi(integers);
-        algorigm(N,integers);
-        System.out.println("integers = " + integers.size());
+        System.out.println("b1 = " + b1);
 
+        if (b && b1) {
+            int algorigm = algorigm(N, integers);
+            System.out.println("가장 높은 오르막길 = " + algorigm);
+
+        }
     }
 
     //수열의 크기 제한
-    public static boolean conditionN(int N) {
+    private static boolean conditionN(int N) {
         if (N >= 1 && N <= 1000) {
             return true;
         }
@@ -25,7 +31,7 @@ public class baek2846 {
     }
 
     //양의정수 제한
-    public static boolean conditionPi(List<Integer> N) {
+    private static boolean conditionPi(List<Integer> N) {
         for (int Ns : N) {
             if (Ns >= 1 && Ns <= 1000) {
                 return true;
@@ -35,28 +41,69 @@ public class baek2846 {
     }
 
     //양의정수 배열로 만들기
-    public static List<Integer> addList(int pi1, int pi2, int pi3, int pi4, int pi5) {
+    private static List<Integer> addList(int pi1, int pi2, int pi3, int pi4, int pi5, int pi6, int pi7, int pi8) {
         List<Integer> addList = new ArrayList<>();
         addList.add(pi1);
         addList.add(pi2);
         addList.add(pi3);
         addList.add(pi4);
         addList.add(pi5);
+        addList.add(pi6);
+        addList.add(pi7);
+        addList.add(pi8);
+
         return addList;
+
+
     }
 
     //오르막길 구하는 알고리즘
-    public static boolean algorigm(int N,List<Integer> integers){
+    private static int algorigm(int N, List<Integer> integers) {
 
-        for (int i = 0; i <integers.size()-1; i++) {
-            Integer integer = integers.get(i);
-            Integer integer1 = integers.get(i+1);
+        int minusValue = minusValue(integers);
 
-            if(integer1-integer>0){
-                return true;
-            }
-        }
-            return false;
+        return minusValue;
     }
 
+
+    private static int minusValue(List<Integer> list) {
+
+        List<Integer> returnValue = new ArrayList<>();
+        int startValue = 0;
+
+        for (int i = 1; i < list.size(); i++) {
+            Integer integer = list.get(startValue);
+
+            int minValue = list.get(i - 1);
+
+            int maxValue = list.get(i);
+
+            if ((maxValue < minValue)) {
+                int minusValue = minValue - integer;
+                startValue = i;
+                returnValue.add(minusValue);
+
+            }
+
+        }
+
+        int i = maxValue(returnValue);
+        return i;
+
+    }
+
+    private static int maxValue(List<Integer> distinctsList) {
+        //값 구하기
+
+        IntSummaryStatistics intSummaryStatistics = distinctsList.stream().mapToInt(Integer::intValue).summaryStatistics();
+        int max = intSummaryStatistics.getMax();
+        return max;
+
+
+    }
+
+
+
 }
+
+
