ea81eb90b9ee79ccf9b1e376489a3971846c1fd4
myblog <myblog0419@naver.com>
2023-08-16 00:45:46 +0900

    프로그래머스 다트 중

--- src/프로그래머스/darts.java
+++ src/프로그래머스/darts.java
@@ -5,43 +5,64 @@ import 백준.Main;
 public class darts {
 
 
-
     public static void main(String[] args) {
         //다트 기회
         int oppertunity = 3;
         //획득 가능 점수
-        int score = 0;
 
-        int s=0;
-        int d=0;
-        int t=0;
+        String dartResult = "1S2D*3T";
+        for (int i = 0; i < dartResult.length(); i++) {
+            //아스키코드
+            int intDartResult = (int) dartResult.charAt(i);
 
-        boolean checkedScore = checkScore(score);
-//        int single = single(s);
-//        int ddouble = ddouble(d);
-//        int triple = triple(t);
+            //숫자 추출, checkedScore가 true 면 숫자
+            int score = resultScore(intDartResult);
+            boolean checkedScore = checkScore(score);
+
+            //sdt 점수 주기
+            sdt(intDartResult);
+
+        }
+
 
     }
 
+    private static int sdt(int intDartResult) {
+        //s83 , d68, t54
+        int squared = 0;
+
+        if (intDartResult == 83) {
+            squared = 1;
+        }
+
+        if (intDartResult == 83) {
+            squared = 2;
+        }
+
+        if (intDartResult == 83) {
+            squared = 3;
+        }
+
+        return squared;
+    }
+
+    private static int resultScore(int intDartResult) {
+        if (intDartResult >= 48 && intDartResult <= 57) {
+            char intDartResult1 = (char) intDartResult;
+            int intResult = Integer.parseInt(String.valueOf(intDartResult1));
+            return intResult;
+        }
+
+        return 11;
+    }
+
     //점수 확인하기
-    public static boolean checkScore(int score){
-        if (score>=0&&score<=10){
+    public static boolean checkScore(int score) {
+        if (score >= 0 && score <= 10) {
             return true;
         }
         return false;
     }
-
-//    public static int single(int s){
-//        Math.pow()
-//    }
-//    public static int ddouble(int d){
-//
-//    }
-//    public int triple(int t){
-//
-//    }
-
-
 
 
 }
