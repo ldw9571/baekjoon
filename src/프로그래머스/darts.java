c35e496fa1773d6929b596e649bc46d59eb8095d
myblog <myblog0419@naver.com>
2023-08-16 20:41:39 +0900

    진짜어렵네

--- src/프로그래머스/darts.java
+++ src/프로그래머스/darts.java
@@ -2,6 +2,10 @@ package 프로그래머스;
 
 import 백준.Main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class darts {
 
 
@@ -9,37 +13,110 @@ public class darts {
         //다트 기회
         int oppertunity = 3;
         //획득 가능 점수
+        int score=0;
 
-        String dartResult = "1S2D*3T";
+        String dartResult = "1S2D3T";
+        List<Integer>numbers = new ArrayList<>();
+        List<Integer>pows = new ArrayList<>();
+        List<Integer>bonus = new ArrayList<>();
+
         for (int i = 0; i < dartResult.length(); i++) {
             //아스키코드
             int intDartResult = (int) dartResult.charAt(i);
 
             //숫자 추출, checkedScore가 true 면 숫자
-            int score = resultScore(intDartResult);
-            boolean checkedScore = checkScore(score);
+            int number = resultScore(intDartResult);
+           // System.out.println("number = " + number);
+            boolean checkedScore = checkScore(number);
 
-            //sdt 점수 주기
-            sdt(intDartResult);
+
+
+            if(checkedScore){
+                numbers.add(number);
+            }
+
+            //sdt 맞나 확인
+            int sdt = sdt(intDartResult);
+           // System.out.println("sdt = " + sdt);
+            boolean sdtIstrue = istrueSdt(intDartResult);
+
+
+            if (sdtIstrue) {
+                //sdt 점수 주기
+                pows.add(sdt);
+
+            }
+
+
+            int doubleOrMinus = checkBonus(intDartResult);
+            bonus.add(doubleOrMinus);
+
 
         }
+
+        for (int j = 0; j <numbers.size() ; j++) {
+            int num = numbers.get(j);
+            System.out.println("num = " + num);
+            int pow = pows.get(j);
+            System.out.println("pow = " + pow);
+
+            int pow1 = (int)Math.pow(num, pow);
+            int result;
+            Integer bonusInteger = bonus.get(j);
+            if(bonusInteger==1){
+                result = pow1 * 2;
+            } else if (bonusInteger==2) {
+                result = score - pow1;
+            }else{
+                result=pow1;
+            }
+
+            System.out.println("result = " + result);
+
+            score+=result;
+            System.out.println("score = " + score);
+
+        }
+
+        System.out.println("Lastscore = " + score);
 
 
     }
 
+    private static int checkBonus(int intDartResult) {
+        //*42, #35
+        if(intDartResult==42){
+            return 1;
+        }
+
+        if (intDartResult==35){
+            return 2;
+        }
+        return 0;
+
+    }
+
+    private static boolean istrueSdt(int intDartResult) {
+        if (intDartResult==83||intDartResult==68||intDartResult==84){
+            return true;
+        }
+        return false;
+
+    }
+
     private static int sdt(int intDartResult) {
-        //s83 , d68, t54
+        //s83 , d68, t84
         int squared = 0;
 
         if (intDartResult == 83) {
             squared = 1;
         }
 
-        if (intDartResult == 83) {
+        if (intDartResult == 68) {
             squared = 2;
         }
 
-        if (intDartResult == 83) {
+        if (intDartResult == 84) {
             squared = 3;
         }
 
@@ -50,6 +127,7 @@ public class darts {
         if (intDartResult >= 48 && intDartResult <= 57) {
             char intDartResult1 = (char) intDartResult;
             int intResult = Integer.parseInt(String.valueOf(intDartResult1));
+
             return intResult;
         }
 
