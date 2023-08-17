d2a497e23929539897e53f29fe2481949db7600b
myblog <myblog0419@naver.com>
2023-08-17 13:26:40 +0900

    palidrome 문제 완료

--- src/프로그래머스/palindrome.java
+++ src/프로그래머스/palindrome.java
@@ -1,31 +1,29 @@
 package 프로그래머스;
 
-import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.List;
 
 public class palindrome {
     public static void main(String[] args) {
-        int min = 100, max = 300;
+        int min = 1, max = 300;
         List<Integer> list = new ArrayList<>();
 
-        makeList(list,min,max);
+        makeList(list, min, max);
+        System.out.print("list = " );
 
         for (int i = 0; i < list.size(); i++) {
-            System.out.println("list = " + list.get(i));
+//            System.out.print(list.get(i)+", ");
         }
-
-
     }
 
 
-    private static void makeList(List<Integer> list,int min, int max){
+    private static void makeList(List<Integer> list, int min, int max) {
         //1~100까지 비교
         for (int i = min; i <= max; i++) {
-
+            System.out.println("i = " + i);
             boolean isTrue = returnBoolean(i);
             if (isTrue) {
-                addList(list,i);
+                addList(list, i);
             }
         }
     }
@@ -33,35 +31,23 @@ public class palindrome {
     private static boolean returnBoolean(int i) {
 
         String num = String.valueOf(i);
-//        System.out.println("num = " + num);
-        int numLength = num.length()-1;
-//        System.out.println("numLength = " + numLength);
-        int compareLength = numLength / 2;
-//        System.out.println("compareLength = " + compareLength);
+        int numLength = num.length() - 1;
 
-        for (int j = 0; j < compareLength; j++, numLength--) {
+        for (int j = 0; j < num.length() / 2; j++, numLength--) {
+            System.out.println("num = " + num);
             char numChar = num.charAt(j);
+            System.out.println("numChar = " + numChar);
             char numCharOpposite = num.charAt(numLength);
-
-            if (numChar == numCharOpposite) {
-//                System.out.println("numChar = " + numChar);
-//                System.out.println("numCharOpposite = " + numCharOpposite);
-
-                return true;
+            System.out.println("numCharOpposite = " + numCharOpposite);
+            if (numChar != numCharOpposite) {
+                return false;
             }
-
-
         }
-        return false;
-
+        return true;
     }
 
-    private static void addList(List<Integer> list,int i) {
+    private static void addList(List<Integer> list, int i) {
         list.add(i);
     }
-
-
-
-
 
 }
