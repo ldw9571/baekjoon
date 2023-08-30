581bae28ec2ee4e418782c5823d8d8cc770cb042
myblog <myblog0419@naver.com>
2023-08-30 18:51:24 +0900

    백준시작

---
+++ src/백준/Baek1200.java
@@ -0,0 +1,9 @@
+package 백준;
+
+public class Baek1200 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/백준/구구단.java
@@ -0,0 +1,11 @@
+package 백준;
+
+public class 구구단 {
+    public static void main(String[] args) {
+        for (int i = 1; i <10 ; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+        }
+    }
+}
