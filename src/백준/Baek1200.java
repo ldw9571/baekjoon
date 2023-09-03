2ecaac6fde057bbbae73e3dadd4449d81153c953
myblog <myblog0419@naver.com>
2023-09-03 21:18:47 +0900

    백준 1003번

--- src/백준/Baek1200.java
+++
@@ -1,9 +0,0 @@
-package 백준;
-
-public class Baek1200 {
-    public static void main(String[] args) {
-
-
-
-    }
-}
--- src/백준/Main.java
+++ src/백준/Main.java
@@ -1,110 +1,39 @@
 package 백준;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    //2845
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T=sc.nextInt();
 
-        //누가 파티에 왔나
-        //몇명이 왔나
-        //정확히 알수는 없다
+        int zero=0;
+        int one=0;
 
-        //1제곱미터당 몇명의 사람이 왔는지 알고 있다
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
 
-        //서로 다른 5개의 신문에 참가자 적혀있음
+            int fibonacci = fibonacci(N);
+            if (fibonacci == 0) {
+                zero = +1;
+            } else if (fibonacci == 1) {
+                one = +1;
+            }
 
-
-        //신문 기사에 실려있는 참가자의 수가 몇명만큼 잘못되어 있는지 구하기
-
-        //1제곱미터당 사람의 수
-        int countBy1Square = 5;
-        //파티가 열렸던 곳의 넓이
-        int squareByParty =20;
-
-        //기사 참가자수 5
-        int article1 = 99,article2 = 101, article3 = 1000, article4 = 0, article5 = 97;
-
-        //기사 참가자수 list에 담아줌
-        List<Integer> integers = articleCount(article1, article2, article3, article4, article5);
-
-        //상근이가 생각하는 사람의 수
-        int countbySangguen = countbySangguen(countBy1Square, squareByParty);
-
-        //상근이가 생각하는 사람의 수 - 기사에 실려있는 참가자의 수
-        List<Integer> variances = variances(countbySangguen, integers);
-
-        int i = 1;
-        for (Integer variance : variances) {
-            System.out.println("상근 - 기사"+i+" 차이 = " + variance);
-            i++;
+            System.out.println("zero = " + zero);
+            System.out.println("one = " + one);
+            zero=0;
+            one=0;
         }
     }
-
-
-
-
-    //파티 넓이 조건
-
-    //상근이가 생각하는 사람의 수
-    public static int countbySangguen(int countBy1Square,int squareByParty){
-        int countBySangguen;
-
-        //인원수 조건 확인
-        boolean trueByPeople = conditionBySquareBypeople(countBy1Square);
-        //파티 면적 조건 확인
-        boolean trueByParty = conditionBySquareByparty(squareByParty);
-
-        if(trueByPeople&&trueByParty) {
-            countBySangguen = countBy1Square * squareByParty;
-            return countBySangguen;
-        }
-        System.out.println("사람수 파티 면적 입력 오류");
-        return 0;
-    }
-
-    //사람수 조건
-    public static boolean conditionBySquareBypeople(int countBy1Square){
-        if(countBy1Square>=1&&countBy1Square<=10){
-            return true;
-        }
-        System.out.println("인원수를 정확히 입력해주세요");
-        return false;
-    }
-    //파티 넓이 조건
-    public static boolean conditionBySquareByparty(int squareByParty){
-        if(squareByParty>=1&&squareByParty<=1000){
-            return true;
-        }
-        System.out.println("파티의 넓이를 정확하게 입력해주세요");
-        return false;
-    }
-
-    //조건에 맞는 5개의 기사 갯수 list
-    public static List<Integer> articleCount(int article1,int article2,int article3,int article4,int article5){
-        List countByPeopleByArticle = new ArrayList<>();
-
-        countByPeopleByArticle.add(article1);
-        countByPeopleByArticle.add(article2);
-        countByPeopleByArticle.add(article3);
-        countByPeopleByArticle.add(article4);
-        countByPeopleByArticle.add(article5);
-
-        return countByPeopleByArticle;
-    }
-
-    //기사 - 상근 구하기
-    public static List<Integer> variances(int countbySangguen, List<Integer> integers){
-        List variances = new ArrayList<>();
-
-        for (Integer integer : integers){
-            int variance = integer - countbySangguen;
-//            int abs = Math.abs(variance);
-            variances.add(variance);
-        }
-
-        return variances;
-    }
-
 }
