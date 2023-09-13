6641d2e25f07f5a99e79bf2b39b202e899680325
myblog <myblog0419@naver.com>
2023-09-13 16:11:41 +0900

    정처기 코드 돌려보기

--- src/test/정처기.java
+++ src/test/정처기.java
@@ -2,21 +2,11 @@ package test;
 
 public class 정처기 {
     public static void main(String[] args) {
-        int w=3, x=4, y=3, z=5;
+       boolean i = false;
+       boolean s = false;
 
-        if((w ==2 | w==y) & !(y>z) & (1==x^y!=z)){
-            w=w+y;
-            if(7==x^y!=w)
-                System.out.println("w = " + w);
-            else
-                System.out.println("x = " + x);
-        }else {
-            w=y+z;
-            if(7== y^z != w)
-                System.out.println("w = " + w);
-        else
-            System.out.println("z = " + z);
-        }
+        boolean b = i || s;
+        System.out.println("b = " + b);
 
     }
 }
